41ae3f6eb55cfb4006a5fcde9d778f7b36d60c08
Skylyxx <skylyxxfr@gmail.com>
2021-05-04 08:04:16 +0200

    ✨Add Betrayal.io game

--- src/config/commands/fun.ts
+++ src/config/commands/fun.ts
@@ -1,6 +1,25 @@
 import { noPermissions, permissions } from "@app/config/configUtil";
 import settings from "@app/config/settings";
 
+export const betrayal = {
+    settings: {
+        aliases: ['betrayal', 'bt'],
+        clientPermissions: permissions.SEND_MESSAGES,
+        userPermissions: noPermissions,
+    },
+    details: {
+        name: 'Betrayal',
+        content: 'Permet de lancer le jeu Betrayal.io dans un vocal',
+        usage: 'betrayal',
+        examples: ['betrayal'],
+    },
+    messages: {
+        notInChannel: `${settings.emojis.non} » Vous devez vous trouver dans un salon vocal ou préciser l'id du salon !`,
+        success: `${settings.emojis.oui} » Très bien ! Voici le lien d'invitation: {link}`
+    },
+    
+}
+
 export const youtube = {
     settings: {
         aliases: ['youtube', 'yt'],
